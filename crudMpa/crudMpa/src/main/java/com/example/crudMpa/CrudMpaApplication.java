db8651b7164bbe9aa19484931b6c423cef1a87af
Gilgamesh2408 <143744111+Gilgamesh2408@users.noreply.github.com>
2023-11-17 21:10:30 -0500

    crudprueba

---
+++ crudMpa/crudMpa/src/main/java/com/example/crudMpa/CrudMpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudMpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMpaApplication.class, args);
+	}
+
+}
