c03030cb5a4a15705b9a4f79a33ba1996e849c07
Gilgamesh2408 <143744111+Gilgamesh2408@users.noreply.github.com>
2023-12-09 10:08:49 -0500

    Delete com.app.crud directory

--- com.app.crud/src/main/java/com/app/crud/com/app/crud/repositorio/LibroRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.app.crud.com.app.crud.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepositorio extends JpaRepository<Libro, Long>{
-
-}
